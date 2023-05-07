ab40f1d0d4f875adca6d6d4bbad0baf33e1f45dd
theodesp <328805+theodesp@users.noreply.github.com>
2023-05-07 17:01:48 +0100

    Feat: Evaluation

--- src/main.zig
+++ src/main.zig
@@ -73,21 +73,68 @@ pub fn main() !void {
 
             // Attempt to Parse the user Input
             var r: c.mpc_result_t = undefined;
-            const isValid = c.mpc_parse("<stdin>", input, Lispy, &r);
+            const isValid = c.mpc_parse("<stdin>", string, Lispy, &r);
             if (isValid == 1) {
-                // On success print and delete the AST
-                c.mpc_ast_print(@ptrCast([*c]c.mpc_ast_t, @alignCast(@alignOf(*c.mpc_ast_t), r.output)));
-                c.mpc_ast_delete(@ptrCast([*c]c.mpc_ast_t, @alignCast(@alignOf(*c.mpc_ast_t), r.output)));
+                const output = @ptrCast([*c]c.mpc_ast_t, @alignCast(@alignOf(*c.mpc_ast_t), r.output));
+                const result = try eval(output);
+
+                // Echo input back to user
+                try stdout.print("{d}\n", .{result});
+                try bw.flush();
+                c.mpc_ast_delete(output);
+                // // On success print and delete the AST
+                // c.mpc_ast_print(output);
+                // c.mpc_ast_delete(output);
             } else {
                 // Otherwise print and delete the Error
                 c.mpc_err_print(r.@"error");
                 c.mpc_err_delete(r.@"error");
             }
-
-            // Echo input back to user
-            try stdout.print("No you're a {s}\n", .{zInput});
-            try bw.flush();
         }
     }
     c.mpc_cleanup(4, Number, Operator, Expr, Lispy);
+}
+
+fn eval(t: *c.mpc_ast_t) !c_long {
+    // If tagged as number return it directly.
+    if (std.mem.indexOf(u8, std.mem.span(t.tag), "number") != null) {
+        return try std.fmt.parseInt(c_long, std.mem.span(t.contents), 10);
+    }
+
+    // The operator is always second child.
+    const op: [*c]u8 = t.children[1].*.contents;
+
+    // We store the third child in `x`
+    var x = try eval(t.children[2]);
+    var i: usize = 3;
+    // Iterate the remaining children and combining.
+    while (true) {
+        const tag = t.children[i].*.tag;
+        if (std.mem.indexOf(u8, std.mem.span(tag), "expr") != null) {
+            const y = try eval(t.children[i]);
+            x = eval_op(x, op, y);
+            i += 1;
+        } else {
+            break;
+        }
+    }
+
+    return x;
+}
+
+// Use operator string to see which operation to perform.
+fn eval_op(x: c_long, op: [*c]u8, y: c_long) c_long {
+    if (std.mem.eql(u8, std.mem.span(op), "+")) {
+        return x + y;
+    }
+    if (std.mem.eql(u8, std.mem.span(op), "-")) {
+        return x - y;
+    }
+    if (std.mem.eql(u8, std.mem.span(op), "*")) {
+        return x * y;
+    }
+    if (std.mem.eql(u8, std.mem.span(op), "/")) {
+        return @divFloor(x, y);
+    }
+    return 0;
 }
