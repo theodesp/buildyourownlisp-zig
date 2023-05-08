2aba4ccd1970640be894351bccc7a79d2c29a1cd
theodesp <328805+theodesp@users.noreply.github.com>
2023-05-08 19:50:45 +0100

    Feat: Basic Error handling

--- src/main.zig
+++ src/main.zig
@@ -79,10 +79,10 @@ pub fn main() !void {
                 const result = try eval(output);
 
                 // Echo input back to user
-                try stdout.print("{d}\n", .{result});
+                try result.println(stdout);
                 try bw.flush();
                 c.mpc_ast_delete(output);
-                // // On success print and delete the AST
+                // On success print and delete the AST
                 // c.mpc_ast_print(output);
                 // c.mpc_ast_delete(output);
             } else {
@@ -95,10 +95,15 @@ pub fn main() !void {
     c.mpc_cleanup(4, Number, Operator, Expr, Lispy);
 }
 
-fn eval(t: *c.mpc_ast_t) !c_long {
+fn eval(t: *c.mpc_ast_t) !Lval {
     // If tagged as number return it directly.
     if (std.mem.indexOf(u8, std.mem.span(t.tag), "number") != null) {
-        return try std.fmt.parseInt(c_long, std.mem.span(t.contents), 10);
+        // Check if there is some error in conversion.
+        if (std.fmt.parseInt(c_long, std.mem.span(t.contents), 10)) |num| {
+            return Lval.init(num);
+        } else |_| {
+            return Lval.init_error(ZLispError.LERR_BAD_NUM);
+        }
     }
 
     // The operator is always second child.
@@ -123,24 +128,93 @@ fn eval(t: *c.mpc_ast_t) !c_long {
 }
 
 // Use operator string to see which operation to perform.
-fn eval_op(x: c_long, op: [*c]u8, y: c_long) c_long {
+fn eval_op(x: Lval, op: [*c]u8, y: Lval) Lval {
+    // If either value is an error return it.
+    if (x.type == LvalType.LVAL_ERR) {
+        return x;
+    }
+    if (y.type == LvalType.LVAL_ERR) {
+        return y;
+    }
+    // Otherwise do maths on the number values.
     if (std.mem.eql(u8, std.mem.span(op), "+")) {
-        return x + y;
+        return Lval.init(x.num + y.num);
     }
     if (std.mem.eql(u8, std.mem.span(op), "-")) {
-        return x - y;
+        return Lval.init(x.num - y.num);
     }
     if (std.mem.eql(u8, std.mem.span(op), "*")) {
-        return x * y;
+        return Lval.init(x.num * y.num);
     }
     if (std.mem.eql(u8, std.mem.span(op), "/")) {
-        return @divFloor(x, y);
+        if (y.num == 0) {
+            // If second operand is zero return error.
+            return Lval.init_error(ZLispError.LERR_DIV_ZERO);
+        }
+        return Lval.init(@divFloor(x.num, y.num));
     }
     if (std.mem.eql(u8, std.mem.span(op), "%")) {
-        return @mod(x, y);
+        return Lval.init(@mod(x.num, y.num));
     }
     if (std.mem.eql(u8, std.mem.span(op), "^")) {
-        return x ^ y;
+        return Lval.init(x.num ^ y.num);
     }
-    return 0;
+    return Lval.init_error(ZLispError.LERR_BAD_OP);
 }
+
+// Create Enumeration of Possible lval Types.
+const LvalType = enum {
+    LVAL_NUM,
+    LVAL_ERR,
+};
+
+// Create Enumeration of Possible Error Types.
+const ZLispError = enum {
+    LERR_DIV_ZERO,
+    LERR_BAD_OP,
+    LERR_BAD_NUM,
+};
+
+// Declare New lval Struct
+const Lval = struct {
+    type: LvalType,
+    num: i64,
+    err: ZLispError,
+
+    // Create a new number type lval.
+    pub fn init(i: i64) Lval {
+        return Lval{ .type = LvalType.LVAL_NUM, .num = i, .err = undefined };
+    }
+    // Create a new error type Lval
+    pub fn init_error(i: ZLispError) Lval {
+        return Lval{ .type = LvalType.LVAL_ERR, .num = undefined, .err = i };
+    }
+    // Print an "Lval".
+    pub fn print(self: Lval, writer: anytype) !void {
+        switch (self.type) {
+            // In the case the type is a number print it.
+            // Then 'break' out of the switch.
+            LvalType.LVAL_NUM => try writer.print("{d}", .{self.num}),
+            // In the case the type is an error.
+            LvalType.LVAL_ERR => {
+                // Check what type of error it is and print it.
+                if (self.err == ZLispError.LERR_DIV_ZERO) {
+                    try writer.print("Error: Division By Zero!", .{});
+                }
+                // Check what type of error it is and print it.
+                if (self.err == ZLispError.LERR_BAD_OP) {
+                    try writer.print("Error: Invalid Operator!", .{});
+                }
+                // Check what type of error it is and print it.
+                if (self.err == ZLispError.LERR_BAD_NUM) {
+                    try writer.print("Error: Invalid Number!", .{});
+                }
+            },
+        }
+    }
+    // Print an Lval followed by a newline.
+    pub fn println(self: Lval, writer: anytype) !void {
+        try self.print(writer);
+        try writer.print("\n", .{});
+    }
+};
